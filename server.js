e9a0bab27a751a07553c2cc25c2a57283aef003d
andersonmcook <andersonmcook@gmail.com>
2016-02-23 15:21:39 -0600

    heroku/mongolab

--- server.js
+++ server.js
@@ -7,6 +7,15 @@ const mongoose = require('mongoose');
 const Question = require('./models/qa.model')
 const PORT = process.env.PORT || 3000;
 
+const MONGODB_HOST = process.env.MONGODB_HOST || 'localhost';
+const MONGODB_PORT = process.env.MONGODB_PORT || 27017;
+const MONGODB_USER = process.env.MONGODB_USER || '';
+const MONGODB_PASS = process.env.MONGODB_PASS || '';
+const MONGODB_DB = 'questionasker';
+const MONGODB_URL_PREFIX = MONGODB_USER ? `${MONGODB_USER}:${MONGODB_PASS}@` : '';
+
+const MONGODB_URL = `mongodb://${MONGODB_URL_PREFIX}${MONGODB_HOST}:${MONGODB_PORT}/${MONGODB_DB}`;
+
 app.set('view engine', 'jade');
 
 app.use(bodyParser.urlencoded({extended: false}));
@@ -23,9 +32,11 @@ app.post('/question', (req, res) => {
 });
 
 app.get('/:question', (req, res) => {
-  console.log(req);
   Question.findOne({url: req.params.question}, (err, data) => {
     if (err) throw err;
+    if (data === null) {
+      res.redirect('/');
+    }
     res.render('answer', {words: data});
   });
 
@@ -33,7 +44,7 @@ app.get('/:question', (req, res) => {
 
 
 // connect
-mongoose.connect('mongodb://localhost:27017/question-answer', (err) => {
+mongoose.connect(MONGODB_URL, (err) => {
   if (err) throw err;
   // listen
   app.listen(PORT, () => {
