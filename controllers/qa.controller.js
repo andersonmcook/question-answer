794f4e63211dad32efccec9c4b06ec487b198ea7
andersonmcook <andersonmcook@gmail.com>
2016-03-11 08:48:32 -0600

    commented out changes

--- controllers/qa.controller.js
+++ controllers/qa.controller.js
@@ -9,17 +9,25 @@ module.exports = {
 // render answer
   renderAnswer (req, res) {
     Question.findOne({url: req.params.question}, (err, data) => {
-      if (err) throw err;
+      if (err) throw err
       if (data === null) {
-        res.redirect('/');
-      } else {
+        res.redirect('/')
+      } else if (data.answerfont === undefined) {
+        // const defaultPres = {
+        //   questioncolor: 'black',
+
+        // }
+
         data.questioncolor === undefined ? data.questioncolor =  'black' : data.questioncolor = data.questioncolor
         data.answercolor === undefined ? data.answercolor =  'black' : data.answercolor = data.answercolor
         data.topcolor === undefined ? data.topcolor =  'white' : data.topcolor = data.topcolor
         data.bottomcolor === undefined ? data.bottomcolor =  'white' : data.bottomcolor = data.bottomcolor
         data.questionfont === undefined ? data.questionfont =  'sans-serif' : data.questionfont = data.questionfont
         data.answerfont === undefined ? data.answerfont =  'sans-serif' : data.answerfont = data.answerfont
-        res.render('answer', {words: data});
+        // data.answerfont = data.answerfont || 'sans-serif'
+        res.render('answer', {words: data})
+      } else {
+        res.render('answer', {words: data})
       }
     })
   },
